1454f5162d4a0e4ffa9903bc72f2f050e26cb6c4
yaacov <kobi.zamir@gmail.com>
2021-03-06 15:37:09 +0200

    initial-upload

---
+++ api/v1beta1/gatetoken_types.go
@@ -0,0 +1,128 @@
+/*
+Copyright 2021 Yaacov Zamir.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// GateTokenCache stores initial token data
+type GateTokenCache struct {
+	From        string `json:"from"`
+	Until       string `json:"until"`
+	DurationSec int64  `json:"duration-sec"`
+	NBf         int64  `json:"nbf"`
+	Exp         int64  `json:"exp"`
+	MatchMethod string `json:"matchMethod"`
+	MatchPath   string `json:"matchPath"`
+	Alg         string `json:"alg"`
+}
+
+// GateTokenSpec defines the desired state of GateToken
+type GateTokenSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// User is the user id of the user requesting this token.
+	// +required
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type="string"
+	// +kubebuilder:validation:MaxLength=226
+	UserID string `json:"user-id"`
+
+	// match-path is a regular expresion used to validate API request path,
+	// API requests matching this pattern will be validated by the token.
+	// This field may not be empty.
+	// +required
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Type="string"
+	// +kubebuilder:validation:Pattern="^/[^:@]+$"
+	// +kubebuilder:validation:MaxLength=1024
+	MatchPath string `json:"match-path"`
+
+	// from is time of token invocation, the token will not validate before this time,
+	// the token duration will start from this time.
+	// Defalut to token object creation time.
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type="string"
+	// +kubebuilder:validation:Format="date-time"
+	From string `json:"from"`
+
+	// duration-sec is the duration in sec the token will be validated since it's invocation.
+	// Defalut value is 3600s (1h).
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type="integer"
+	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:default:=3600
+	DurationSec int64 `json:"duration-sec"`
+
+	// match-path is a comma seperated list of allowed http methods,
+	// only API requests matching one of the allowed methods will be validated.
+	// Defalut value is "GET,OPTIONS".
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Type="string"
+	// +kubebuilder:validation:Pattern="^(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE)+(,(GET|HEAD|POST|PUT|DELETE|CONNECT|OPTIONS|TRACE)+)*$"
+	// +kubebuilder:validation:MaxLength=1024
+	// +kubebuilder:default:="GET,OPTIONS"
+	MatchMethod string `json:"match-method"`
+}
+
+// GateTokenStatus defines the observed state of GateToken
+type GateTokenStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Conditions represent the latest available observations of an object's state
+	Conditions []metav1.Condition `json:"conditions"`
+
+	// The generated token
+	Token string `json:"token"`
+
+	// Cached data, once created, user can not change this valuse
+	Data GateTokenCache `json:"data"`
+
+	// Token generation phase (ready|error)
+	Phase string `json:"phase"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// GateToken is the Schema for the gatetokens API
+type GateToken struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GateTokenSpec   `json:"spec,omitempty"`
+	Status GateTokenStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// GateTokenList contains a list of GateToken
+type GateTokenList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GateToken `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&GateToken{}, &GateTokenList{})
+}
---
+++ controllers/gatetoken_controller.go
@@ -0,0 +1,227 @@
+/*
+Copyright 2021 Yaacov Zamir.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	ocgatev1beta1 "github.com/yaacov/oc-gate-operator/api/v1beta1"
+)
+
+// GateTokenReconciler reconciles a GateToken object
+type GateTokenReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=ocgate.yaacov.com,resources=gatetokens,verbs=get;list;watch;create;delete
+// +kubebuilder:rbac:groups=ocgate.yaacov.com,resources=gatetokens/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=ocgate.yaacov.com,resources=gatetokens/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the GateToken object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
+func (r *GateTokenReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = r.Log.WithValues("gatetoken", req.NamespacedName)
+
+	// your logic here
+
+	// Lookup the GateToken instance for this reconcile request
+	token := &ocgatev1beta1.GateToken{}
+	if err := r.Get(ctx, req.NamespacedName, token); err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			r.Log.Info("GateToken resource not found. Ignoring since object must be deleted.")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		r.Log.Error(err, "Failed to get GateToken.")
+		return ctrl.Result{}, err
+	}
+
+	// If token was created, exit.
+	if token.Status.Phase != "" {
+		r.Log.Info("stale token", "id", token.Name)
+		return ctrl.Result{}, nil
+	}
+
+	// Parse and cache user data.
+	if err := cacheData(token); err != nil {
+		r.Log.Info("Can't parse token data", "err", err)
+
+		setErrorCondition(token, "UserDataError", err)
+
+		if err := r.Status().Update(ctx, token); err != nil {
+			r.Log.Info("Failed to update status", "err", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// Get private key secret
+	key, err := getSecret(ctx, r.Client, "oc-gate-jwt-secret", token.Namespace)
+	if err != nil {
+		r.Log.Info("Can't read private key secret", "err", err)
+
+		setErrorCondition(token, "PrivateKeyError", err)
+		if err := r.Status().Update(ctx, token); err != nil {
+			r.Log.Info("Failed to update status", "err", err)
+		}
+
+		return ctrl.Result{}, nil
+	}
+
+	// Create token
+	err = singToken(token, key)
+	if err != nil {
+		r.Log.Info("Can't read private key secret", "err", err)
+
+		setErrorCondition(token, "PrivateKeyError", err)
+		if err := r.Status().Update(ctx, token); err != nil {
+			r.Log.Info("Failed to update status", "err", err)
+		}
+
+		return ctrl.Result{}, nil
+	}
+
+	// Token is ready
+	setReadyCondition(token, "TokenCreated", "token created")
+	if err := r.Status().Update(ctx, token); err != nil {
+		r.Log.Info("Failed to update status", "err", err)
+	}
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *GateTokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&ocgatev1beta1.GateToken{}).
+		Complete(r)
+}
+
+// Cache user data
+func cacheData(token *ocgatev1beta1.GateToken) error {
+	var nbf int64
+
+	if token.Spec.From == "" {
+		nbf = int64(time.Now().Unix())
+	} else {
+		t, err := time.Parse(time.RFC3339, token.Spec.From)
+		if err != nil {
+			return err
+		}
+		nbf = int64(t.Unix())
+	}
+
+	token.Status.Data = ocgatev1beta1.GateTokenCache{
+		NBf:         nbf,
+		Exp:         nbf + int64(token.Spec.DurationSec),
+		From:        token.Spec.From,
+		Until:       time.Unix(nbf+int64(token.Spec.DurationSec), 0).UTC().Format(time.RFC3339),
+		DurationSec: token.Spec.DurationSec,
+		MatchMethod: token.Spec.MatchMethod,
+		MatchPath:   token.Spec.MatchPath,
+		Alg:         jwt.SigningMethodRS256.Name,
+	}
+
+	return nil
+}
+
+func getSecret(ctx context.Context, client client.Client, name string, namespace string) ([]byte, error) {
+	// Get private key secret
+	secret := &corev1.Secret{}
+	namespaced := &types.NamespacedName{
+		Name:      name,
+		Namespace: namespace,
+	}
+
+	if err := client.Get(ctx, *namespaced, secret); err != nil {
+		return nil, err
+	}
+
+	key := secret.Data["key.pem"]
+	return key, nil
+}
+
+func setErrorCondition(token *ocgatev1beta1.GateToken, reason string, err error) {
+	t := metav1.Time{Time: time.Now()}
+	token.Status.Phase = "Error"
+	condition := metav1.Condition{
+		Type:               "Error",
+		Status:             "True",
+		Reason:             reason,
+		Message:            fmt.Sprintf("%s", err),
+		LastTransitionTime: t,
+	}
+	token.Status.Conditions = []metav1.Condition{condition}
+}
+
+func setReadyCondition(token *ocgatev1beta1.GateToken, reason string, message string) {
+	t := metav1.Time{Time: time.Now()}
+	token.Status.Phase = "Ready"
+	condition := metav1.Condition{
+		Type:               "Ready",
+		Status:             "True",
+		Reason:             reason,
+		Message:            message,
+		LastTransitionTime: t,
+	}
+	token.Status.Conditions = []metav1.Condition{condition}
+}
+
+func singToken(token *ocgatev1beta1.GateToken, key []byte) error {
+	// Create token
+	claims := &jwt.MapClaims{
+		"exp":         token.Status.Data.Exp,
+		"nbf":         token.Status.Data.NBf,
+		"matchPath":   token.Status.Data.MatchPath,
+		"matchMethod": token.Status.Data.MatchMethod,
+	}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	jwtKey, err := jwt.ParseRSAPrivateKeyFromPEM(key)
+	if err != nil {
+		return err
+	}
+	out, err := jwtToken.SignedString(jwtKey)
+	if err != nil {
+		return err
+	}
+
+	token.Status.Token = out
+	return nil
+}
